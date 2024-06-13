881b72addd000563ca731c5d73e2444be56bddb2
hypeprogrammer <goodmangil@knou.ac.kr>
2024-06-13 16:56:15 +0900

    gitignore 파일 생성

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from app import create_app
+from flask import Flask
+from app.controllers.prediction_controller import prediction_bp
 
-app = create_app()
+app = Flask(__name__)
+app.register_blueprint(prediction_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
