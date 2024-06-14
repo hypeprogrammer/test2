f6f838f6385166d0635c3617f147b251362bce64
hypeprogrammer <goodmangil@knou.ac.kr>
2024-06-14 11:23:30 +0900

    main.py에 메인 컨트롤러 기능 추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# main.py
 from flask import Flask
 from app.controllers.prediction_controller import prediction_bp
+from app.controllers.main_controller import main_bp
 
 app = Flask(__name__)
 app.register_blueprint(prediction_bp, url_prefix='/api')
+app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
