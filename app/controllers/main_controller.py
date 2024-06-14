2fb652ca7f6aaed2791ab31ca9ac6f5d0d483313
hypeprogrammer <goodmangil@knou.ac.kr>
2024-06-14 11:21:12 +0900

    index.html을 렌더링하는 컨트롤러 추가

---
+++ app/controllers/main_controller.py
@@ -0,0 +1,8 @@
+# app/controllers/main_controller.py
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
